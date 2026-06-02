d7f1963d594e729edc7fd93c3734cd8c15e9c8c4
dieulinhhoang <dieuleen1211@gmail.com>
2026-06-02 20:45:17 +0700

    Revert "feat: add RBAC permissions decorator, guard, and role permission assignment APIs"
    This reverts commit 8be96a31d7cec6c4f58faf18f0d2c90c8eb9d777.

--- src/role/role.module.ts
+++ src/role/role.module.ts
@@ -2,24 +2,11 @@ import { Module } from '@nestjs/common';
 import { RoleService } from './role.service';
 import { RoleController } from './role.controller';
 import { Role } from 'src/database/entities/role.entity';
-import { Permission } from 'src/database/entities/permission.entity';
-import { RolePermission } from 'src/database/entities/role-permission.entity';
-import { GroupPermission } from 'src/database/entities/group-permission.entity';
-import { UserRole } from 'src/database/entities/user-role.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   controllers: [RoleController],
   providers: [RoleService],
-  imports: [
-    TypeOrmModule.forFeature([
-      Role,
-      Permission,
-      RolePermission,
-      GroupPermission,
-      UserRole,
-    ]),
-  ],
-  exports: [RoleService],
+  imports: [TypeOrmModule.forFeature([Role])],
 })
 export class RoleModule {}
